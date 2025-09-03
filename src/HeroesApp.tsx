a2aebe0dea9fbb718f9fdf77bcbf456f7802e6cc
FrancoDev7 <francodevs01@gmail.com>
2025-09-03 17:34:02 -0400

    Contexto Favoritos

--- src/HeroesApp.tsx
+++ src/HeroesApp.tsx
@@ -2,17 +2,17 @@ import { RouterProvider } from "react-router";
 import { AppRouter } from "./router/app.router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { FavoriteHeroProvider } from "./heroes/context/FavoriteHeroContext";
 
 const queryClient = new QueryClient();
 
 export const HeroesApp = () => {
   return (
-    <>
-      <QueryClientProvider client={queryClient}>
+    <QueryClientProvider client={queryClient}>
+      <FavoriteHeroProvider>
         <RouterProvider router={AppRouter} />
-
         <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-    </>
+      </FavoriteHeroProvider>
+    </QueryClientProvider>
   );
 };
