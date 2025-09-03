2ba9515986ff555b53c4733b9f54e34a39bd1b88
FrancoDev7 <francodevs01@gmail.com>
2025-09-03 16:17:41 -0400

    sort import

--- src/router/app.router.tsx
+++ src/router/app.router.tsx
@@ -1,10 +1,10 @@
 import { lazy } from "react";
 import { createBrowserRouter, Navigate } from "react-router";
 
-import { HeroPage } from "@/heroes/pages/hero/HeroPage";
+import { AdminLayout } from "@/admin/layouts/AdminLayout";
 import { AdminPage } from "@/admin/pages/AdminPage";
 import { HeroesLayout } from "@/heroes/layouts/HeroesLayout";
-import { AdminLayout } from "@/admin/layouts/AdminLayout";
+import { HeroPage } from "@/heroes/pages/hero/HeroPage";
 import { HomePage } from "@/heroes/pages/home/HomePage";
 
 //import { SearchPage } from "@/heroes/pages/search/SearchPage";
