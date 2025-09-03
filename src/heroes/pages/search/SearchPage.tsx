12cf9b491908b3db25c5722ee4e2b8d9c891a89b
FrancoDev7 <francodevs01@gmail.com>
2025-09-03 19:00:53 -0400

    Buscar heroes

--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -1,9 +1,23 @@
+import { useSearchParams } from "react-router";
+import { useQuery } from "@tanstack/react-query";
+
+import { CustomBreadCrumbs } from "@/components/custom/CustomBreadCrumbs";
 import { CustomHeader } from "@/components/custom/CustomHeader";
+import { HeroGrid } from "@/heroes/components/HeroGrid";
 import { HeroStats } from "@/heroes/components/HeroStats";
 import { SearchControls } from "./ui/SearchControls";
-import { CustomBreadCrumbs } from "@/components/custom/CustomBreadCrumbs";
+import { searchHeroesAction } from "@/heroes/actions/search-heroes.action";
 
 export const SearchPage = () => {
+  const [searchParams] = useSearchParams();
+  const name = searchParams.get("name") ?? undefined;
+
+  const { data: heroes = [] } = useQuery({
+    queryKey: ["search", { name }],
+    queryFn: () => searchHeroesAction({ name }),
+    staleTime: 1000 * 60 * 5, // 5 Minutos
+  });
+
   return (
     <>
       <CustomHeader
@@ -22,6 +36,8 @@ export const SearchPage = () => {
       <HeroStats />
       {/* Filter Search  */}
       <SearchControls />
+
+      <HeroGrid heroes={heroes} />
     </>
   );
 };
